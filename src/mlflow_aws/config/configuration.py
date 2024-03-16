88ba1634107b4991de6e4d4c7afc240f54a973d0
Mohmmad Hadain <hadan@Hadans-MacBook-Pro.local>
2024-03-16 20:10:39 +0530

    changed dagshub endpoint

--- src/mlflow_aws/config/configuration.py
+++ src/mlflow_aws/config/configuration.py
@@ -103,7 +103,7 @@ class ConfigurationManager:
             all_params=params,
             metric_file_name = config.metric_file_name,
             target_column = schema.name,
-            mlflow_uri="https://dagshub.com/migumax/mlflow_aws.mlflow",
+            mlflow_uri="https://dagshub.com/hadainn/mlops-cicd",
            
         )
 
